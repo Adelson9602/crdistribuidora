cc3bd11c3d3c3d6e8287667d42505647efb5be4a
Elver Rodriguez <adelson9602@gmail.com>
2021-05-26 23:09:48 -0500

    Se agrega los tabs y tabs-panels

--- src/pages/Warehouse/Articles.vue
+++ src/pages/Warehouse/Articles.vue
@@ -1,12 +1,44 @@
 <template>
-  <q-page class="flex flex-center">
-    <p class="text">ARTICULOS</p>
+  <q-page padding>
+     <q-card class="height-card_page">
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-primary"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="articles" label="Articulos" icon="inventory_2"/>
+          <q-tab name="create_article" label="Agregar articulo" icon="add_business"/>
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="articles">
+            <div class="text-h6">Mails</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="create_article">
+            <div class="text-h6">Alarms</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'Articles'
+  name: 'Articles',
+  data(){
+    return {
+      tab: 'articles'
+    }
+  }
 }
 </script>
 
