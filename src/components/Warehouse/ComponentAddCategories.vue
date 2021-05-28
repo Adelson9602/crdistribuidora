9f327037ef2452405a897bf0789324d2b21d0519
Elver Rodriguez <adelson9602@gmail.com>
2021-05-27 22:48:21 -0500

    Se crea el componente que contendra el fomulario para gregar la categoría y se agrega el fomrulario al componente

---
+++ src/components/Warehouse/ComponentAddCategories.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <q-btn color="primary" icon="add" label="agregar categoria" @click="dialog_create_categories = true" />
+    <q-dialog v-model="dialog_create_categories" persistent>
+      <q-card style="width: 700px; max-width: 80vw;">
+        <q-form
+          @submit="onSubmit"
+          @reset="onReset"
+          class="q-gutter-md"
+        >
+          <q-card-section class="row items-center">
+            <q-avatar icon="add" color="primary" text-color="white" />
+            <span class="q-ml-sm text-h6">Agregar categoría</span>
+          </q-card-section>
+          <q-card-section class="row">
+            <div class="col-xs-12 col-md-6 q-px-sm">
+              <q-input
+                v-model="text"
+                type="text"
+                hint="Nombre"
+                :rules="[val => !!val || 'Nombre es obligatorio']"
+              />
+            </div>
+            <div class="col-xs-12 col-md-6 q-px-sm">
+              <q-input
+                v-model="text"
+                type="text"
+                hint="Descripción"
+                :rules="[val => !!val || 'Descripción es obligatorio']"
+              />
+            </div>
+          </q-card-section>
+          <q-card-actions align="right" class="q-px-md">
+            <q-btn flat label="Cancelar" color="primary" v-close-popup />
+            <q-btn label="Agregar" color="primary" type="submit"/>
+          </q-card-actions>
+        </q-form>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentAddCategories',
+  data () {
+    return {
+      dialog_create_categories: false,
+      text: null
+    }
+  },
+  methods: {
+    onSubmit(){
+
+    },
+    onReset(){
+
+    },
+  }
+}
+</script>
