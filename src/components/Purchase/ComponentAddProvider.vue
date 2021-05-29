3ad7c25fe16b9b372cbd9e37f9c1e5914e34c0c0
Elver Rodriguez <adelson9602@gmail.com>
2021-05-28 22:57:27 -0500

    Se agrega inputs y sus validaciones

---
+++ src/components/Purchase/ComponentAddProvider.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <div class="row">
+        <div class="col-xs-12 col-md-3">
+          <q-input
+            v-model="text"
+            type="text"
+            hint="Razon social"
+            :rules="[val => !!val || 'Razon social es obligatorio']"
+          />
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-input
+            v-model="text"
+            type="text"
+            hint="Nombre"
+            :rules="[val => !!val || 'Nombre es obligatorio']"
+          />
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-select
+            v-model="model"
+            :options="options"
+            hint="Tipo documento"
+            :rules="[val => !!val || 'Tipo documento es obligatorio']"
+          />
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-input
+            v-model="text"
+            type="text"
+            hint="Número documento"
+            :rules="[val => !!val || 'Número documento es obligatorio']"
+          />
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-input
+            v-model="text"
+            type="text"
+            hint="Dirección"
+            :rules="[val => !!val || 'Dirección es obligatorio']"
+          />
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-input
+            v-model="text"
+            type="text"
+            hint="Teléfono"
+            :rules="[val => !!val || 'Teléfono es obligatorio']"
+          />
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-input
+            v-model="text"
+            type="text"
+            hint="Email"
+            :rules="[val => !!val || 'Email es obligatorio']"
+          />
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-select
+            v-model="model"
+            :options="options"
+            hint="Ciudad"
+            :rules="[val => !!val || 'Ciudad es obligatorio']"
+          />
+        </div>
+      </div>
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentAddProvider',
+  data () {
+    return {
+      model: null,
+      options: [],
+      text: null
+    }
+  },
+  methods:{
+    onSubmit(){
+
+    },
+    onReset(){
+
+    },
+  }
+}
+</script>
