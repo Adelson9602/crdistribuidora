9c0829a9ced3660563c3cc6b8dd4878f3ac1b890
Elver Rodriguez <adelson9602@gmail.com>
2021-05-28 22:07:17 -0500

    Se agrega input

---
+++ src/components/Purchase/ComponentAddIncome.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <div class="row">
+        <div class="col-xs-12 col-md-3 q-px-sm">
+          <q-select v-model="model" :options="options" hint="Proveedor" />
+        </div>
+        <div class="col-xs-12 col-md-3 q-px-sm">
+          <q-select v-model="model" :options="options" hint="Tipo comprobante" />
+        </div>
+        <div class="col-xs-12 col-md-3 q-px-sm">
+          <q-select v-model="model" :options="options" hint="medio de pago" />
+        </div>
+        <div class="col-xs-12 col-md-3 q-px-sm">
+          <q-input v-model="text" type="text" hint="Número compra" />
+        </div>
+        <div class="col-xs-12 col-md-3 q-px-sm">
+          <q-input v-model="text" type="text" hint="Inpuesta" />
+        </div>
+        <div class="col-xs-12 col-md-3 q-px-sm">
+          <q-field hint="Rounded filled" stack-label>
+            <template v-slot:control>
+              <div class="self-center full-width no-outline" tabindex="0"></div>
+            </template>
+          </q-field>
+        </div>
+      </div>
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  data () {
+    return {
+      model: null,
+      options: [],
+      text: null
+    }
+  },
+  methods:{
+    onSubmit(){
+
+    },
+    onReset(){
+
+    },
+  }
+}
+</script>
