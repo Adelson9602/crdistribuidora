9cbb2bf58dc5726b410c990d64a10a26d0433864
Elver Rodriguez <adelson9602@gmail.com>
2021-05-28 22:15:47 -0500

    Se agrega validaciones de los inputs y selects, se agrega tabla ejemplo

--- src/components/Purchase/ComponentAddIncome.vue
+++ src/components/Purchase/ComponentAddIncome.vue
@@ -7,44 +7,207 @@
     >
       <div class="row">
         <div class="col-xs-12 col-md-3 q-px-sm">
-          <q-select v-model="model" :options="options" hint="Proveedor" />
+          <q-select
+            v-model="model"
+            :options="options"
+            hint="Proveedor"
+            :rules="[validateSelect]"
+          />
         </div>
         <div class="col-xs-12 col-md-3 q-px-sm">
-          <q-select v-model="model" :options="options" hint="Tipo comprobante" />
+          <q-select
+            v-model="model"
+            :options="options"
+            hint="Tipo comprobante"
+            :rules="[validateSelect]"
+          />
         </div>
         <div class="col-xs-12 col-md-3 q-px-sm">
-          <q-select v-model="model" :options="options" hint="medio de pago" />
+          <q-select
+            v-model="model"
+            :options="options"
+            hint="medio de pago"
+            :rules="[validateSelect]"
+          />
         </div>
         <div class="col-xs-12 col-md-3 q-px-sm">
-          <q-input v-model="text" type="text" hint="Número compra" />
+          <q-input
+            v-model="text"
+            type="text"
+            hint="Númerocompra"
+            :rules="[val => !!val || 'Númerocompra es obligatorio']"
+          />
         </div>
         <div class="col-xs-12 col-md-3 q-px-sm">
-          <q-input v-model="text" type="text" hint="Inpuesta" />
+          <q-input
+            v-model="text"
+            type="text"
+            hint="Impuesto"
+            :rules="[val => !!val || 'Impuesto es obligatorio']"
+          />
         </div>
         <div class="col-xs-12 col-md-3 q-px-sm">
-          <q-field hint="Rounded filled" stack-label>
+          <q-field hint="Fecha" stack-label>
             <template v-slot:control>
-              <div class="self-center full-width no-outline" tabindex="0"></div>
+              <div class="self-center full-width no-outline" tabindex="0">28/05/2021</div>
             </template>
           </q-field>
         </div>
       </div>
       <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        <q-btn label="Submit" type="submit" color="primary" class="hide-btn_submit"/>
       </div>
     </q-form>
+    <div class="row">
+      <div class="col-xs-12">
+        <q-table
+          title="Inventario"
+          :data="data"
+          :columns="columns"
+          row-key="name"
+          flat
+        >
+          <template v-slot:header-cell-calories="props">
+            <q-th :props="props">
+              <q-icon name="thumb_up" size="1.5em" />
+              {{ props.col.label }}
+            </q-th>
+          </template>
+        </q-table>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  // name: 'ComponentName',
+  name: 'ComponentAddIncome',
   data () {
     return {
       model: null,
       options: [],
-      text: null
+      text: null,
+      columns: [
+        {
+          name: 'name',
+          required: true,
+          label: 'Dessert (100g serving)',
+          align: 'left',
+          field: row => row.name,
+          format: val => `${val}`,
+          sortable: true
+        },
+        { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+        { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+        { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+        { name: 'protein', label: 'Protein (g)', field: 'protein' },
+        { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+        { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+        { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+      ],
+      data: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%'
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          sodium: 129,
+          calcium: '8%',
+          iron: '1%'
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          sodium: 337,
+          calcium: '6%',
+          iron: '7%'
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          sodium: 413,
+          calcium: '3%',
+          iron: '8%'
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          sodium: 327,
+          calcium: '7%',
+          iron: '16%'
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          sodium: 50,
+          calcium: '0%',
+          iron: '0%'
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          sodium: 38,
+          calcium: '0%',
+          iron: '2%'
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          sodium: 562,
+          calcium: '0%',
+          iron: '45%'
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          sodium: 326,
+          calcium: '2%',
+          iron: '22%'
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          sodium: 54,
+          calcium: '12%',
+          iron: '6%'
+        }
+      ]
     }
   },
   methods:{
@@ -54,6 +217,12 @@ export default {
     onReset(){
 
     },
+    // Valida los select
+    validateSelect(val){
+      if(!val){
+        return 'Por favor seleccione una opción'
+      }
+    }
   }
 }
 </script>
