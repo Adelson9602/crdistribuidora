1f0438f54f0694018275335f2e33f5a18c861425
unknown <oscars.santamaria@gmail.com>
2021-06-03 09:23:24 -0500

    se configura  el dev en quasar config y el axios

--- quasar.conf.js
+++ quasar.conf.js
@@ -45,7 +45,12 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
+      env:{
 
+        __ENV__: 'dev',
+        __VERSION__: '2.21.6.3',
+        __URLAPI__: `http://apis.crdistribuidora.com/pcr/`,
+      },
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,14 @@
-import Vue from 'vue'
 import axios from 'axios'
 
-Vue.prototype.$axios = axios
+export default ({Vue}) => {
+    let baseURL = null;
+    // Validamos el entorno de la aplicación, para asignar la url de la api, si es dev estamos en desarrollo caso trario en producción
+    if(process.env.__ENV__ === 'dev'){
+      baseURL = 'https://apis.crdistribuidora.com/pcr/';
+    } else if (process.env.__ENV__ === 'production'){
+      baseURL = process.env.__URLAPI__;
+    }
+    Vue.prototype.$axios = axios.create({
+      baseURL
+    });
+  }
