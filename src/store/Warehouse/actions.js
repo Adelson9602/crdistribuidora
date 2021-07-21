e590be7b4d094ef286918bef2d8bed11efb445be
Elver Rodriguez <adelson9602@gmail.com>
2021-07-21 11:15:37 -0500

    Se cambia petición y se da formato al codigo

--- src/store/Warehouse/actions.js
+++ src/store/Warehouse/actions.js
@@ -1,18 +1,13 @@
-/*
-export function someAction (context) {
-}
-*/
-
 import Vue from 'vue';
 
-// Lista todos los productos agrupados por articulos y discriminados por ventas
-export function getDataArticles(contex, data){
-    return Vue.prototype.$axios.get(`select_articulo_ten/${data.base}`);
-  }
+//consulta general de articulos trae todos los reasultados
+export function getAllArticles(){
+  return Vue.prototype.$axios.get(`select_articulo_general/${process.env.__BASE__}`)
+}
 
-   // Lista todos los productos agrupados por articulos y discriminados por ventas
-export function requestgetDataArticlesRange(contex, data){
-  return Vue.prototype.$axios.get(`select_articulo_range/${data.base}/${data.from}/${data.to}`)
+// Lista todos los productos agrupados por articulos y discriminados por ventas
+export function requestgetDataArticlesRange(contex, data) {
+  return Vue.prototype.$axios.get(`select_articulo_range/${process.env.__BASE__}/${data.from}/${data.to}`)
 }
 
 
