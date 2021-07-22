19a70daef116d1d80ddbd09a130a365ca91fd25a
Elver Rodriguez <adelson9602@gmail.com>
2021-07-22 10:34:58 -0500

    Se incluye la store

--- src/store/index.js
+++ src/store/index.js
@@ -25,7 +25,8 @@ export default function (/* { ssrContext } */) {
       sales,
       warehouse,
       auth,
-      access
+      access,
+      master
     },
 
     // enable strict mode (adds overhead!)
