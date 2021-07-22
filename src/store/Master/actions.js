57987809fce771e8c701205203c815214055f987
Elver Rodriguez <adelson9602@gmail.com>
2021-07-22 10:32:06 -0500

    Se agrega petición para obtener todas las unidades de medidas

--- src/store/Master/actions.js
+++ src/store/Master/actions.js
@@ -1,4 +1,6 @@
-/*
-export function someAction (context) {
+import Vue from "vue";
+
+// Obtiene las unidades de medida
+export function getAllUm(){
+    return Vue.prototype.$axios.get(`select_unidad_medida/${process.env.__BASE__}`);
 }
-*/
