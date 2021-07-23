e0304d5a6d6b2bf9402059dc7232542c629172a1
Elver Rodriguez <adelson9602@gmail.com>
2021-07-23 13:09:24 -0500

    Se agrega objeto para almacenar el encabezado del traslado y se cambia la validación de los selects

--- src/components/Warehouse/ComponentDoTransfer.vue
+++ src/components/Warehouse/ComponentDoTransfer.vue
@@ -11,7 +11,7 @@
             v-model="model"
             :options="options"
             hint="Tipo de traslado"
-            :rules="[validateSelect]"
+            :rules="[val => !!val || 'Tipo de traslado es requerido']"
           />
         </div>
         <div class="col-xs-12 col-md-4 q-px-sm">
@@ -19,7 +19,7 @@
             v-model="model"
             :options="options"
             hint="Origen"
-            :rules="[validateSelect]"
+            :rules="[val => !!val || 'Origen es requerido']"
           />
         </div>
         <div class="col-xs-12 col-md-4 q-px-sm">
@@ -27,7 +27,7 @@
             v-model="model"
             :options="options"
             hint="Destino"
-            :rules="[validateSelect]"
+            :rules="[val => !!val || 'Destino es requerido']"
           />
         </div>
       </div>
@@ -59,8 +59,9 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
-  // name: 'ComponentName',
+  name: 'ComponentDoTransfer',
   data () {
     return {
       model: null,
@@ -71,140 +72,31 @@ export default {
         'Option 4',
         'Option 5',
       ],
-      columns: [
-        {
-          name: 'name',
-          required: true,
-          label: 'Dessert (100g serving)',
-          align: 'left',
-          field: row => row.name,
-          format: val => `${val}`,
-          sortable: true
-        },
-        { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
-        { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
-        { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
-        { name: 'protein', label: 'Protein (g)', field: 'protein' },
-        { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
-        { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
-        { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
-      ],
-      data: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          sodium: 87,
-          calcium: '14%',
-          iron: '1%'
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          sodium: 129,
-          calcium: '8%',
-          iron: '1%'
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          sodium: 337,
-          calcium: '6%',
-          iron: '7%'
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          sodium: 413,
-          calcium: '3%',
-          iron: '8%'
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          sodium: 327,
-          calcium: '7%',
-          iron: '16%'
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          sodium: 50,
-          calcium: '0%',
-          iron: '0%'
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          sodium: 38,
-          calcium: '0%',
-          iron: '2%'
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          sodium: 562,
-          calcium: '0%',
-          iron: '45%'
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          sodium: 326,
-          calcium: '2%',
-          iron: '22%'
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          sodium: 54,
-          calcium: '12%',
-          iron: '6%'
-        }
-      ]
+      columns: [],
+      data: [],
+      enc_traslado: {
+        base: null,
+        Etm_Id: null,
+        Etm_Mov_ID_entrega: null,
+        Etm_Usuario_entrega: null,
+        Etm_Fecha_entrega: null,
+        Etm_Mov_Id_recibe: null,
+        Etm_Usuario_recibe: null,
+        Etm_Fecha_recibe: null,
+        Etm_Observaciones: null,
+        Etm_Estado: null
+      }
     }
   },
   methods: {
+    ...mapActions('warehouse', [
+      ''
+    ]),
     onSubmit(){
 
     },
     onReset(){
 
-    },
-    // Valida si el select tiene alguna opción
-    validateSelect(val) {
-      if (val === null) {
-        return "Campo obligatorio, debe seleccionar una opción";
-      }
     },
   }
 }
