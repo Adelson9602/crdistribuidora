1d6e43d1967ebdce3c38df9a4ff745b9e963dcb7
Elver Rodriguez <adelson9602@gmail.com>
2021-07-23 15:05:13 -0500

    Se agrega petición para obtener las moviles

--- src/store/Master/actions.js
+++ src/store/Master/actions.js
@@ -1,6 +1,13 @@
 import Vue from "vue";
 
+let base = process.env.__BASE__;
+
 // Obtiene las unidades de medida
 export function getAllUm(){
-    return Vue.prototype.$axios.get(`select_unidad_medida/${process.env.__BASE__}`);
+    return Vue.prototype.$axios.get(`select_unidad_medida/${base}`);
+}
+
+// Obtiene las moviles
+export function getMovil(){
+    return Vue.prototype.$axios.get(`select_movil_general/${base}`)
 }
