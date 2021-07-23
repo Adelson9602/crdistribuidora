2ba5867c6c51e9ce32c05128a5b36851280cea45
Elver Rodriguez <adelson9602@gmail.com>
2021-07-23 15:06:01 -0500

    se agrega petición para obtner el inventario general o movil

--- src/store/Warehouse/actions.js
+++ src/store/Warehouse/actions.js
@@ -1,18 +1,20 @@
 import Vue from 'vue';
 
+let base = process.env.__BASE__;
+
 //consulta general de articulos trae todos los reasultados
 export function getAllArticles(){
-  return Vue.prototype.$axios.get(`select_articulo_general/${process.env.__BASE__}`)
+  return Vue.prototype.$axios.get(`select_articulo_general/${base}`)
 }
 
 // Lista todos los productos agrupados por articulos y discriminados por ventas
 export function requestgetDataArticlesRange(contex, data) {
-  return Vue.prototype.$axios.get(`select_articulo_range/${process.env.__BASE__}/${data.from}/${data.to}`)
+  return Vue.prototype.$axios.get(`select_articulo_range/${base}/${data.from}/${data.to}`)
 }
 
 // Lista las categorias
 export function getCategoriasAlmacen(){
-  return Vue.prototype.$axios.get(`select_categoria_general/${process.env.__BASE__}`);
+  return Vue.prototype.$axios.get(`select_categoria_general/${base}`);
 }
 
 // Agrega o edita los articulos
@@ -29,15 +31,26 @@ export function addCategory(contex, data){
 
 // Obtiene los 50 ultimos traslados realizados
 export function getTransfer(){
-  return Vue.prototype.$axios.get(`select_enc_traslado_moviles_limit/${process.env.__BASE__}`);
+  return Vue.prototype.$axios.get(`select_enc_traslado_moviles_limit/${base}`);
 }
 
 // Obtiene los traslados realizados filtrados por rango de fecha
 export function getTransferRange(contex, data){
-  return Vue.prototype.$axios.get(`select_enc_traslado_moviles_range/${process.env.__BASE__}/${data.Fecha_inicial}/${data.Fecha_final}`);
+  return Vue.prototype.$axios.get(`select_enc_traslado_moviles_range/${base}/${data.Fecha_inicial}/${data.Fecha_final}`);
 }
 
 // Inserta o actualiza el encabezado del traslado entre bodegas
 export function insertEncTransfer(contex, data){
   return Vue.prototype.$axios.post(`insert_update_enc_traslado_moviles`, data);
+}
+
+// Inventario
+// Obtiene todo el inventario en general
+export function getStock(){
+  return Vue.prototype.$axios.get(`select_stock_inventario_general/${base}`);
+}
+
+// Obtiene el inventario de una movil
+export function getStockMovil(contex, id_movil){
+  return Vue.prototype.$axios.get(`select_stock_inventario_single/${base}/${id_movil}`)
 }
