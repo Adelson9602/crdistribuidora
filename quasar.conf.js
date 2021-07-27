6d4c37c69361cfb0d7f80033165c442f235219ea
Elver Rodriguez <adelson9602@gmail.com>
2021-07-27 15:04:15 -0500

    Se cambia a modo history

--- quasar.conf.js
+++ quasar.conf.js
@@ -44,7 +44,7 @@ module.exports = function (/* ctx */) {
 
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       env:{
         __ENV__: 'dev',
         __VERSION__: '2.21.6.3',
