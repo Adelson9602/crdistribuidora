5c8f5c681f4dee0f6935ca5e9eb8d0fd65940e77
Elver Rodriguez <adelson9602@gmail.com>
2021-07-27 16:08:51 -0500

    Se cambia numero

--- src/components/Generals/ComponentTable.vue
+++ src/components/Generals/ComponentTable.vue
@@ -204,7 +204,7 @@
                     >
                       <q-fab-action
                         :color="
-                          props.row.Estado == 0 ? 'red' : 'green'
+                          props.row.Estado == 1 ? 'red' : 'green'
                         "
                         round
                         push
