48cae20e728cf93e56ce7ed15079371acca7c16d
Elver Rodriguez <adelson9602@gmail.com>
2021-08-08 23:25:53 -0500

    Se agrega peticiones para obtener items, guardar items y modulos

--- src/store/Access/actions.js
+++ src/store/Access/actions.js
@@ -1,5 +1,26 @@
 import Vue from "vue";
 let base = process.env.__BASE__;
+// PERMISOS
+//Inicio Select Login
+export function GetModules (context) {
+    return Vue.prototype.$axios.get(`list_modulos_frontend/${base}`);
+}
+
+// Insertar módulos
+export function PostCreateModules (context, data) {
+    return Vue.prototype.$axios.post('insert_modulos_frontend',data);
+}
+
+// Traer items X módulos
+export function GetModulesHasItem (context) {
+    return Vue.prototype.$axios.get(`list_man_modulo_has_item_frontend/${base}`);
+}
+
+// Insertar items X módulos
+export function PostCreateItemModules (context, data) {
+    return Vue.prototype.$axios.post('insert_item_modulo_frontend',data);
+}
+
 // Obtiene personal en general
 export function getPersons (context) {
     return Vue.prototype.$axios.get(`select_personal_general/${base}`)
