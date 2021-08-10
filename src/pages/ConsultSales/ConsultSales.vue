a2184ed45c35a25f9db079a1b70b04f2cf78dbe2
Elver Rodriguez <adelson9602@gmail.com>
2021-08-09 23:51:05 -0500

    Se agrega petición para obtener las ventas de un cliente

--- src/pages/ConsultSales/ConsultSales.vue
+++ src/pages/ConsultSales/ConsultSales.vue
@@ -52,142 +52,72 @@ export default {
         {
           name: 'CP_Nit',
           align: 'center',
-          label: 'CP_Nit',
+          label: 'NIT',
           sortable: true,
           field: 'CP_Nit',
         },
         {
           name: 'CP_Razon_social',
           align: 'center',
-          label: 'CP_Razon_social',
+          label: 'Razón social',
           sortable: true,
           field: 'CP_Razon_social',
         },
         {
           name: 'Ev_Des_total_art',
           align: 'center',
-          label: 'Ev_Des_total_art',
+          label: 'Descuento total articulo',
           sortable: true,
           field: 'Ev_Des_total_art',
         },
         {
           name: 'Ev_Des_gen_venta',
           align: 'center',
-          label: 'Ev_Des_gen_venta',
+          label: 'Descuento general',
           sortable: true,
           field: 'Ev_Des_gen_venta',
         },
         {
           name: 'Ev_Descuentog',
           align: 'center',
-          label: 'Ev_Descuentog',
+          label: 'Descuento',
           sortable: true,
           field: 'Ev_Descuentog',
         },
         {
-          name: 'Ev_Entregado',
-          align: 'center',
-          label: 'Ev_Entregado',
-          sortable: true,
-          field: 'Ev_Entregado',
-        },
-        {
-          name: 'Ev_Fecha_control',
-          align: 'center',
-          label: 'Ev_Fecha_control',
-          sortable: true,
-          field: 'Ev_Fecha_control',
-        },
-        {
-          name: 'Ev_Fecha_venta',
-          align: 'center',
-          label: 'Ev_Fecha_venta',
-          sortable: true,
-          field: 'Ev_Fecha_venta',
-        },
-        {
           name: 'Ev_Impuesto',
           align: 'center',
-          label: 'Ev_Impuesto',
+          label: 'Impuesto',
           sortable: true,
           field: 'Ev_Impuesto',
         },
         {
           name: 'Ev_Subtotal',
           align: 'center',
-          label: 'Ev_Subtotal',
+          label: 'Subtotal',
           sortable: true,
           field: 'Ev_Subtotal',
         },
         {
           name: 'Ev_Total_venta',
           align: 'center',
-          label: 'Ev_Total_venta',
+          label: 'Total venta',
           sortable: true,
           field: 'Ev_Total_venta',
         },
         {
-          name: 'Ev_conf_pago',
+          name: 'Ev_Fecha_venta',
           align: 'center',
-          label: 'Ev_conf_pago',
+          label: 'Fecha venta',
           sortable: true,
-          field: 'Ev_conf_pago',
+          field: 'Ev_Fecha_venta',
         },
         {
-          name: 'Ev_dias_credito',
+          name: 'Estado',
           align: 'center',
-          label: 'Ev_dias_credito ',
+          label: 'Estado',
           sortable: true,
-          field: 'Ev_dias_credito',
-        },
-        {
-          name: 'Mov_Descripcion',
-          align: 'center',
-          label: 'Mov_Descripcion',
-          sortable: true,
-          field: 'Mov_Descripcion',
-        },
-        {
-          name: 'name_estado',
-          align: 'center',
-          label: 'name_estado',
-          sortable: true,
-          field: 'name_estado',
-        },
-        {
-          name: 'name_mp',
-          align: 'center',
-          label: 'name_mp',
-          sortable: true,
-          field: 'name_mp',
-        },
-        {
-          name: 'Per_Nombre',
-          align: 'center',
-          label: 'Per_Nombre',
-          sortable: true,
-          field: 'Per_Nombre',
-        },
-        {
-          name: 'Per_Num_documento',
-          align: 'center',
-          label: 'Per_Num_documento',
-          sortable: true,
-          field: 'Per_Num_documento',
-        },
-        {
-          name: 'name_qautorizqa',
-          align: 'center',
-          label: 'name_qautorizqa',
-          sortable: true,
-          field: 'name_qautorizqa',
-        },
-        {
-          name: 'Ev_Estado',
-          align: 'center',
-          label: 'Ev_Estado',
-          sortable: true,
-          field: 'Ev_Estado',
+          field: 'Estado',
         },
       ],
       data: [],
@@ -207,7 +137,84 @@ export default {
   watch: {
     client_selected(value){
       if(value){
-        console.log(value)
+        this.$q.loading.show({
+          message: 'Obteniendo ventas del cliente, por favor espere...',
+        });
+        setTimeout(async() => {
+          try {
+            const res_client = await this.getSalesClient(value).then( res => {
+              return res.data;
+            });
+            console.log({
+              msg: 'Respuesta get ventas al cliente',
+              data: res_client
+            });
+            if(res_client.ok){
+              if(res_client.result){
+                this.data.length = 0;
+                res_client.data.forEach( venta => {
+                  this.data.push({
+                    CP_Nit: venta.CP_Nit,
+                    CP_Razon_social: venta.CP_Razon_social,
+                    Ev_Des_gen_venta: venta.Ev_Des_gen_venta,
+                    Ev_Des_total_art: venta.Ev_Des_total_art,
+                    Ev_Descuentog: venta.Ev_Descuentog,
+                    Ev_Entregado: venta.Ev_Entregado,
+                    Ev_Estado: venta.Ev_Estado,
+                    Ev_Fecha_control: venta.Ev_Fecha_control,
+                    Ev_Fecha_venta: venta.Ev_Fecha_venta,
+                    Ev_Id: venta.Ev_Id,
+                    Ev_Impuesto: venta.Ev_Impuesto,
+                    Ev_Subtotal: venta.Ev_Subtotal,
+                    Ev_Total_venta: venta.Ev_Total_venta,
+                    Ev_conf_pago: venta.Ev_conf_pago,
+                    Ev_dias_credito: venta.Ev_dias_credito,
+                    Mov_Descripcion: venta.Mov_Descripcion,
+                    Mov_Id: venta.Mov_Id,
+                    Mp_Id: venta.Mp_Id,
+                    Per_Nombre: venta.Per_Nombre,
+                    Per_Num_documento: venta.Per_Num_documento,
+                    Estado: venta.name_estado,
+                    status: venta.Ev_Estado,
+                    title: venta.CP_Razon_social,
+                    name_mp: venta.name_mp,
+                    name_qautorizqa: venta.name_qautorizqa,
+                    // btn_edit: true,
+                    // btn_status: true,
+                    btn_details: true,
+                    // btn_pdf: true,
+                    // icon_btn_edit: "mdi-pencil",
+                    // icon_btn_status: "power_settings_new",
+                    icon_btn_details: "mdi-eye-settings",
+                  })
+                });
+              } else {
+                this.$q.notify({
+                  message: 'Sin resultados',
+                  type: 'warning'
+                })
+              }
+            } else {
+              throw new Error(res_client.message);
+            }
+          } catch (e) {
+            console.log(e);
+            if (e.message === "Network Error") {
+              e = e.message;
+            }
+            if (e.message === "Request failed with status code 404") {
+              e = "URL de solicitud no existe, err 404";
+            } else if (e.message) {
+              e = e.message;
+            }
+            this.$q.notify({
+              message: e,
+              type: "negative",
+            });
+          } finally {
+            this.$q.loading.hide();
+          }
+        }, 2000)
       }
     }
   },
@@ -216,7 +223,8 @@ export default {
   },
   methods: {
     ...mapActions('salesinquiries', [
-      'getSales'
+      'getSales',
+      'getSalesClient'
     ]),
     ...mapActions('shopping', [
       'getProviders'
@@ -241,12 +249,6 @@ export default {
                 this.data.push({
                   CP_Nit: venta.CP_Nit,
                   CP_Razon_social: venta.CP_Razon_social,
-                  Eg_Fecha_control: venta.Eg_Fecha_control,
-                  Eg_Id: venta.Eg_Id,
-                  Eg_Observacion: venta.Eg_Observacion,
-                  Eg_Quien_autoriza: venta.Eg_Quien_autoriza,
-                  Eg_User_control: venta.Eg_User_control,
-                  Eg_estado: venta.Eg_estado,
                   Ev_Des_gen_venta: venta.Ev_Des_gen_venta,
                   Ev_Des_total_art: venta.Ev_Des_total_art,
                   Ev_Descuentog: venta.Ev_Descuentog,
@@ -265,9 +267,18 @@ export default {
                   Mp_Id: venta.Mp_Id,
                   Per_Nombre: venta.Per_Nombre,
                   Per_Num_documento: venta.Per_Num_documento,
-                  name_estado: venta.name_estado,
+                  Estado: venta.name_estado,
+                  status: venta.Ev_Estado,
+                  title: venta.CP_Razon_social,
                   name_mp: venta.name_mp,
                   name_qautorizqa: venta.name_qautorizqa,
+                  // btn_edit: true,
+                  // btn_status: true,
+                  btn_details: true,
+                  // btn_pdf: true,
+                  // icon_btn_edit: "mdi-pencil",
+                  // icon_btn_status: "power_settings_new",
+                  icon_btn_details: "mdi-eye-settings",
                 })
               });
             } else {
--- src/store/SalesInquiries/actions.js
+++ src/store/SalesInquiries/actions.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
 let base = process.env.__BASE__;
-export function getSales (context) {
+
+// Obtiene las ventas en general
+export function getSales() {
   return Vue.prototype.$axios.get(`select_enc_venta_general/${base}`);
 }
 
+// Obtiene las ventas de un cliente
+export function getSalesClient(context, nit){
+  return Vue.prototype.$axios.get(`select_enc_venta_general_cliente/${base}/${nit}`)
+}
