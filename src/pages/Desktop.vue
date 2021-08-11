744f15fd2019be73aa4e363ec6b129462b71c2fe
Elver Rodriguez <adelson9602@gmail.com>
2021-08-11 11:43:39 -0500

    Se agrega graficos

--- src/pages/Desktop.vue
+++ src/pages/Desktop.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="q-pa-md">
+  <div class="q-pa-md" id="chart">
+    <!-- CARDS -->
     <div class="row q-pt-sm content-card">
       <div class="q-pa-md col-xs-12 col-sm-6 col-md-3">
         <q-card class="my-card">
@@ -114,15 +115,162 @@
         </q-card>
       </div>
     </div>
+    <!-- CHARTS -->
+    <div class="row q-gutter-y-md">
+      <div class="col-xs-12 col-sm-6 col-md-8 q-px-md">
+        <apexchart type="line" height="350" :options="options_line" :series="data_line" />
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-4 q-px-md">
+        <apexchart type="area" height="350" :options="options_area" :series="data_area" />
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-6 q-px-md">
+        <apexchart type="bar" height="350" :options="options_bar" :series="data_bar" />
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-6 q-px-md">
+        <apexchart type="bar" height="350" :options="options_line" :series="data_line" />
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import { mapActions, mapMutations, mapState } from "vuex";
-
+import VueApexCharts from 'vue-apexcharts';
 export default {
+  components: {
+    apexchart: VueApexCharts
+  },
   data() {
     return {
-      
+      data_line: [{
+        name: 'Sales',
+        data: [4, 3, 10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5]
+      }],
+      options_line: {
+        chart: {
+          height: 350,
+          type: 'line',
+        },
+        forecastDataPoints: {
+          count: 7
+        },
+        stroke: {
+          width: 5,
+          curve: 'smooth'
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: ['1/11/2000', '2/11/2000', '3/11/2000', '4/11/2000', '5/11/2000', '6/11/2000', '7/11/2000', '8/11/2000', '9/11/2000', '10/11/2000', '11/11/2000', '12/11/2000', '1/11/2001', '2/11/2001', '3/11/2001','4/11/2001' ,'5/11/2001' ,'6/11/2001'],
+          tickAmount: 10,
+          labels: {
+            formatter: function(value, timestamp, opts) {
+              return opts.dateFormatter(new Date(timestamp), 'dd MMM')
+            }
+          }
+        },
+        title: {
+          text: 'Forecast',
+          align: 'left',
+          style: {
+            fontSize: "16px",
+            color: '#666'
+          }
+        },
+        fill: {
+          type: 'gradient',
+          gradient: {
+            shade: 'dark',
+            gradientToColors: [ '#FDD835'],
+            shadeIntensity: 1,
+            type: 'horizontal',
+            opacityFrom: 1,
+            opacityTo: 1,
+            stops: [0, 100, 100, 100]
+          },
+        },
+        yaxis: {
+          min: -10,
+          max: 40
+        }
+      },
+      data_area: [{
+        name: 'series1',
+        data: [31, 40, 28, 51, 42, 109, 100]
+      }, {
+        name: 'series2',
+        data: [11, 32, 45, 32, 34, 52, 41]
+      }],
+      options_area: {
+        chart: {
+          height: 350,
+          type: 'area'
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+        },
+        tooltip: {
+          x: {
+            format: 'dd/MM/yy HH:mm'
+          },
+        },
+      },
+      data_bar: [
+        {
+          name: 'Net Profit',
+          data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+        }, {
+          name: 'Revenue',
+          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+        }, {
+          name: 'Free Cash Flow',
+          data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+        }
+      ],
+      options_bar: {
+        chart: {
+          type: 'bar',
+          height: 350
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '55%',
+            endingShape: 'rounded'
+          },
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ['transparent']
+        },
+        xaxis: {
+          categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+        },
+        yaxis: {
+          title: {
+            text: '$ (thousands)'
+          }
+        },
+        fill: {
+          opacity: 1
+        },
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return "$ " + val + " thousands"
+            }
+          }
+        }
+      },
     };
   },
   computed: {
