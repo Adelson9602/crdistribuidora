74fde979144d0a80df85bd3272f66ef445573470
Elver Rodriguez <adelson9602@gmail.com>
2021-08-13 22:15:11 -0500

    Se agrega petición para ventas por rango de fecha

--- src/store/Movements/actions.js
+++ src/store/Movements/actions.js
@@ -12,6 +12,11 @@ export function getSalesClient(context, nit){
   return Vue.prototype.$axios.get(`select_enc_venta_general_cliente/${base}/${nit}`)
 }
 
+// Obtiene las ventas por rango de fecha
+export function getSalesClientRange(context, data){  
+  return Vue.prototype.$axios.get(`select_enc_venta_general_cliente_range/${base}/${data.nit}/${data.from}/${data.to}`)
+}
+
 // Obtiene el detalle de una venta
 export function getDetailSales(context, id_venta){
   return Vue.prototype.$axios.get(`select_det_venta_single/${base}/${id_venta}`)
