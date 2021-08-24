80364ace8bf5113707c69e9ddc6bbca13eaa9c8b
Elver Rodriguez <adelson9602@gmail.com>
2021-08-24 16:34:47 -0500

    Se agrega petición para obtener datos para las graficas

--- src/store/Desktop/actions.js
+++ src/store/Desktop/actions.js
@@ -1,4 +1,11 @@
-/*
-export function someAction (context) {
+import Vue from "vue";
+
+let base = process.env.__BASE__;
+
+export function chartProductMoreSales(){
+    return Vue.prototype.$axios.get(`select_graf_product_plus_sale/${base}`)
 }
-*/
+
+export function chartDailySales(){
+    return Vue.prototype.$axios.get(`select_graf_sale_diari/${base}`)
+}
