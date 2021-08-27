b8ee625f25301951903b133b53e5a97d5041b120
Elver Rodriguez <adelson9602@gmail.com>
2021-08-27 14:24:37 -0500

    Se da formato al archivo y se agrega petición para crear la movil e ingresar integrantes a la movil

--- src/store/Master/actions.js
+++ src/store/Master/actions.js
@@ -3,58 +3,68 @@ import Vue from "vue";
 let base = process.env.__BASE__;
 
 // Obtiene las unidades de medida
-export function getAllUm(){
+export function getAllUm() {
     return Vue.prototype.$axios.get(`select_unidad_medida/${base}`);
 }
 
 // Obtiene las moviles
-export function getMovil(){
+export function getMovil() {
     return Vue.prototype.$axios.get(`select_movil_general/${base}`)
 }
 
 // Obtiene los integrantes de una movil
-export function getMembersMovil(conetext, id_movil){
+export function getMembersMovil(conetext, id_movil) {
     return Vue.prototype.$axios.get(`select_movil_integrante_single/${base}/${id_movil}`);
 }
 
 // Tipos de comprobante
-export function getTiposComprobante(){
+export function getTiposComprobante() {
     return Vue.prototype.$axios.get(`select_tipo_comprobante/${base}`);
 }
 
 // medio de pago
-export function getMedioPago(){
+export function getMedioPago() {
     return Vue.prototype.$axios.get(`select_medio_de_pago/${base}`)
 }
 
 // Obtiene todas las ciudades
-export function getCities(){
+export function getCities() {
     return Vue.prototype.$axios.get(`select_ciudad_general/${base}`)
 }
 
 // Obtiene todos los roles
-export function getRol(){
+export function getRol() {
     return Vue.prototype.$axios.get(`select_rol_general/${base}`);
 }
 
 // Obtiene los cargos
-export function getCargos(){
+export function getCargos() {
     return Vue.prototype.$axios.get(`select_cargos_general/${base}`)
 }
 
 
 //consulta general de porcentajes  trae todos los reasultados
-export function getAllPorcentaje(){
+export function getAllPorcentaje() {
     return Vue.prototype.$axios.get(`select_porcentaje_venta_general/${base}`)
-  }
-  
-  // Agrega o edita los porcentajes
-  export function addPorcentaje(contex, data){
-      return Vue.prototype.$axios.post(`insert_update_porcentaje_venta`, data);
-    }
+}
 
-      // Agrega o edita los cargos
-  export function addCargos(contex, data){
+// Agrega o edita los porcentajes
+export function addPorcentaje(contex, data) {
+    return Vue.prototype.$axios.post(`insert_update_porcentaje_venta`, data);
+}
+
+// Agrega o edita los cargos
+export function addCargos(contex, data) {
     return Vue.prototype.$axios.post(`insert_update_cargos`, data);
-  }
-  
+}
+
+// MOVILES
+// Crea la movil
+export function insertMovil(context, data){
+    return Vue.prototype.$axios.post(`insert_update_movil`, data);
+}
+
+// Crea los integrantes de la movil
+export function insertIntegranteMovil(context, data){
+    return Vue.prototype.$axios.post(`insert_update_movil_integrante`, data);
+}
